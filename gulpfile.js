d0dfbbeecd904715e332eff1d4dc1630367dd366
Nadezhda Ray <nadezhdaray37@gmail.com>
2018-01-18 18:12:59 +0300

    [chore] update main page

--- gulpfile.js
+++ gulpfile.js
@@ -3,38 +3,24 @@
 const gulp = require('gulp');
 const autoprefixer = require('gulp-autoprefixer');
 const imagemin = require('gulp-imagemin');
-
-let cleanCSS = require('gulp-clean-css');
-let uglify = require('gulp-uglify');
-let pump = require('pump');
 let pngquant = require('imagemin-pngquant');
-let eslint = require( 'gulp-eslint' );
-let fs = require( 'gulp-fs' );
-let rimraf = require('rimraf');
 
 let sass = require('gulp-sass');
 let less = require('gulp-less');
 
 /* Add browsers' prefixes */
 gulp.task('autoprefixer', () =>
-  gulp.src('./pages/flex/_prj_sass/css/style.css')
+  gulp.src('./pages/grid/_prj_sass/css/style.css')
     .pipe(autoprefixer({
       browsers: ['last 2 versions'],
       cascade: false
     }))
-    .pipe(gulp.dest('./pages/flex/_prj_sass/css'))
+    .pipe(gulp.dest('./pages/grid/_prj_sass/css'))
 );
 
 /* Clean the assembly */
 gulp.task('clean', function (cb) {
   rimraf('./build', cb);
-});
-
-/* Compress *.css files */
-gulp.task('css-min', () => {
-  return gulp.src('./pages/flex/_prj_sass/css/style.css')
-    .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(gulp.dest('./pages/flex/_prj_sass/css/v1'));
 });
 
 /* Compress *.jpeg/*.png files */
@@ -49,7 +35,6 @@ gulp.task('image-min', () =>
     .pipe(gulp.dest('./pages/grid/_prj_sass/images/v1'))
 );
 
-/* Sass/SCSS -> CSS */
 gulp.task('sass', ['autoprefixer'], () => {
   return gulp.src('./app/pages/grid/prj_sass/sass/style.scss')
     .pipe(sass())
